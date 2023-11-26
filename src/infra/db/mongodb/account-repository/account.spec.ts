e3ede9d285a57a0cd7db842d3fb4dda50d47c52f
pedrinhosj <joaopsj007@gmail.com>
2023-11-26 19:33:49 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,32 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { MongoHelper } from '../helpers/mongo-helper';
+import { AccountMongoRepository } from './account';
+
+describe('Account Mongo Repository', () => {
+  let mongo: MongoMemoryServer
+  beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri();
+    await MongoHelper.connect(uri)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.clear()
+    await MongoHelper.disconnect()
+    await mongo.stop()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+})
