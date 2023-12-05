290bfd48c23fa7e066a16b41cc555c875a5d4a89
pedrinhosj <joaopsj007@gmail.com>
2023-12-05 10:37:00 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,31 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { LogMongoRepository } from './log'
+
+describe('Log Mongo Repository', () => {
+  let mongo: MongoMemoryServer
+  let errorCollection: Collection
+  beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const uri = mongo.getUri();
+    await MongoHelper.connect(uri)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongo.stop()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
